0997dea845df26233dfc09c8f32d551ca1cdf5c9
auramaya720 <auramaya720@gmail.com>
2025-12-17 22:20:02 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,1 @@
-int main() {
-    Node* root = NULL;
 
-    bacaCSV(root);
-
-    cout << "Mahasiswa Lulus:\n";
-    tampilkanMahasiswaLulus(root, 75);
-
-    cout << "\nRata-rata keseluruhan: "
-         << hitungRataNilai(root) << endl;
-
-    return 0;
-}
